401d8cfa955b424f580614c6f70ebc9699c412d9
ThotaMac16 <mymac@ThotaMac16s-MacBook-Pro.local>
2021-04-20 15:06:21 +0800

    new weather status

--- 1_weather/src/App.js
+++ 1_weather/src/App.js
@@ -3,22 +3,18 @@ import './index.css';
 import { pathOr, propOr, head } from 'ramda'; // importing ramda for safechecks
 
 // API config to connect openweahtermap.org
-
 const api = {
   key: "97ef8ae65f9917e0b9377c89b4d83a6a",
   base: "https://api.openweathermap.org/data/2.5/"
 }
 
-
 function App() {
 
   // Default state
-
   const [query, setQuery] = useState('');
   const [weather, setWeather] = useState('');
 
   // API call to openweather
-
   const search = evt => {
     if (evt.key == "Enter") {
       fetch(`${api.base}weather?q=${query}&units=metric&appid=${api.key}`)
@@ -35,7 +31,6 @@ function App() {
    * @param {date} d // takes current date in Date format
    * @returns date in format Tuesday 20 April 2021
    */
-
   const dateBuilder = (d) => {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -50,7 +45,6 @@ function App() {
   }
 
   // propOr, pathOr, head are utilities from ramda
-
   const city = propOr('Singapore', 'name')(weather);
   const country = pathOr('SG', ['sys', 'country'])(weather);
   const temperature = pathOr(0, ['main', 'temp'])(weather);
@@ -67,7 +61,7 @@ function App() {
             onChange={e => setQuery(e.target.value)}
             value={query}
             onKeyPress={search}
-            placeholder="Search..." />
+            placeholder="Search city..." />
         </div>
         <div className="location-box">
           <div className="location">{city}, {country}</div>
